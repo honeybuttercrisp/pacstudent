f8b99f5c516b76876441e4cd39897cd86d5212e8
honeybuttercrisp <vietdragon.jv@gmail.com>
2024-09-26 14:54:49 +1000

    added audio assets and audiomanager script

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip introMusic;
+    public AudioClip normalGhostsMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.volume = 0.2f;
+        PlayIntroMusic();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlayIntroMusic()
+    {
+        audioSource.clip = introMusic;
+        audioSource.Play();
+        Invoke("PlayNormalGhostsMusic", introMusic.length);
+    }
+
+    void PlayNormalGhostsMusic()
+    {
+        audioSource.clip = normalGhostsMusic;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+}
